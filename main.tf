7035f95f030dd15d6951545c3c4d1c04f74e63ee
Mihikatfcode86 <dev87aks.ngp1mihika@outlook.com>
2026-09-07 23:28:01 +0530

    Configure Terraform provider and update resource group
    Added required providers and updated resource group details.

--- main.tf
+++ main.tf
@@ -1,14 +1,22 @@
-resource "azurerm_resource_group" "dev" {
-
-name = "rg-dv-terraform"
-
-location = "Eas US"
- 
-tags = {
-
-Environment = "Dev"
-
-ManagedBy = "Terraform"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "dev" {
+  name     = "rg-dev-terraform"
+  location = "East US"
+
+  tags = {
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
 }
