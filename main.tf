a9e29849ed65736614eebcc87ee24b553a74b88a
Mihikatfcode86 <dev87aks.ngp1mihika@outlook.com>
2026-09-08 22:51:45 +0530

    Refactor resource group to use variables
    Updated resource group configuration to use variables for name, location, and tags.

--- main.tf
+++ main.tf
@@ -11,12 +11,10 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "dev" {
-  name     = "rg-dev-terraform"
-  location = "East US"
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 
-  tags = {
-    Environment = "Dev"
-    ManagedBy   = "Terraform"
-  }
+  tags = var.tags
+}
 }
