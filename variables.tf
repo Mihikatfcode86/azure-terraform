2bffb4aaa2c0ad123085b35bd3ea5b7bf47fe53b
Mihikatfcode86 <dev87aks.ngp1mihika@outlook.com>
2026-09-08 22:54:35 +0530

    Add Terraform variables for resource configuration

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+}
 
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment Name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Resource Tags"
+  type        = map(string)
+}
