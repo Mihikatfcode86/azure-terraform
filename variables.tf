caa180582d21e1396834de7c685ab3da98a51538
Mihikatfcode86 <dev87aks.ngp1mihika@outlook.com>
2026-09-17 17:45:21 +0530

    Refactor variables in variables.tf for consistency

--- variables.tf
+++ variables.tf
@@ -1,19 +1,71 @@
-variable "resource_group_name" {
-  description = "Resource Group Name"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure Region"
+variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
 }
 
 variable "environment" {
-  description = "Environment Name"
+  description = "Environment name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource Group name"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Virtual Network name"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "VNet address space"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "Subnet name"
+  type        = string
+}
+
+variable "subnet_address_prefix" {
+  description = "Subnet address prefix"
+  type        = list(string)
+}
+
+variable "vm_name" {
+  description = "RHEL VM name"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Azure VM size"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Administrator username"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key"
+  type        = string
+  sensitive   = true
+}
+
+variable "allowed_ssh_source" {
+  description = "Source CIDR permitted to SSH to the VM"
   type        = string
 }
 
 variable "tags" {
-  description = "Resource Tags"
+  description = "Resource tags"
   type        = map(string)
+  default     = {}
 }
