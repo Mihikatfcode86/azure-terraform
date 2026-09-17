b61a67acc11c3ba174a46ea0d2c7453befdeadd1
Mihikatfcode86 <dev87aks.ngp1mihika@outlook.com>
2026-09-17 17:55:41 +0530

    Refactor variables in variables.tf
    Removed unused variables and updated resource group name variable.

--- variables.tf
+++ variables.tf
@@ -3,18 +3,13 @@ variable "subscription_id" {
   type        = string
 }
 
-variable "environment" {
-  description = "Environment name"
+variable "resource_group_name" {
+  description = "Resource Group name"
   type        = string
 }
 
 variable "location" {
   description = "Azure region"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Resource Group name"
   type        = string
 }
 
@@ -30,42 +25,4 @@ variable "vnet_address_space" {
 
 variable "subnet_name" {
   description = "Subnet name"
-  type        = string
-}
-
-variable "subnet_address_prefix" {
-  description = "Subnet address prefix"
-  type        = list(string)
-}
-
-variable "vm_name" {
-  description = "RHEL VM name"
-  type        = string
-}
-
-variable "vm_size" {
-  description = "Azure VM size"
-  type        = string
-}
-
-variable "admin_username" {
-  description = "Administrator username"
-  type        = string
-}
-
-variable "ssh_public_key" {
-  description = "SSH public key"
-  type        = string
-  sensitive   = true
-}
-
-variable "allowed_ssh_source" {
-  description = "Source CIDR permitted to SSH to the VM"
-  type        = string
-}
-
-variable "tags" {
-  description = "Resource tags"
-  type        = map(string)
-  default     = {}
-}
+  type        = 
